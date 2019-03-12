a9b1f2936ef991ddc1e5596bd1073004714f37bf
ggdizon <47364332+ggdizon@users.noreply.github.com>
2019-03-12 09:28:33 -0600

    Add files via upload
    removed obsolete statements

--- Source-code/MainGame.java
+++ Source-code/MainGame.java
@@ -6,19 +6,16 @@ import java.awt.image.BufferStrategy;
 
 public class MainGame extends Canvas implements Runnable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 501367441143464273L;
 
 	public static final int WIDTH = 500, HEIGHT = (int) (500 * 1.6);
-	
+
 	private Thread thread;
 	private boolean running = false;
 	private Handler handler;
 	private Board board;
 	private Shape shape;
-	
+
 	public MainGame () {
 		shape = new Shape();
 		board = new Board();
@@ -27,16 +24,16 @@ public class MainGame extends Canvas implements Runnable {
 		board.placeShape(shape);
 		handler = new Handler();
 		this.addKeyListener(new KeyInput(handler, board, shape));
-		
+
 		new Window(WIDTH + 120, HEIGHT + 180, "Tetros KMS", this);
 	}
-	
+
 	public synchronized void start() {
 		thread = new Thread(this);
 		thread.start();
 		running = true;
 	}
-	
+
 	public synchronized void stop() {
 		try {
 			thread.join();
@@ -46,7 +43,7 @@ public class MainGame extends Canvas implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
@@ -66,7 +63,7 @@ public class MainGame extends Canvas implements Runnable {
 				render();
 				frames++;
 			}
-			
+
 			if (System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
 				// System.out.println("FPS: " + frames);
@@ -106,43 +103,42 @@ public class MainGame extends Canvas implements Runnable {
 		}
 		stop();
 	}
-	
+
 	private void tick() {
 		handler.tick();
 	}
-	
+
 	private void render() {
 		BufferStrategy bs = this.getBufferStrategy();
 		if (bs == null) {
 			this.createBufferStrategy(3);
 			return;
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
+
 		g.setColor(Color.GRAY);
 		g.fillRect(25, 25, WIDTH, HEIGHT);
 		g.setColor(Color.BLACK);
-		for (int i = WIDTH/10 + 25; i <= WIDTH; i += WIDTH/10) 
+		for (int i = WIDTH/10 + 25; i <= WIDTH; i += WIDTH/10)
 			g.fillRect(i-1, 25, 2, HEIGHT);
 		for (int j = HEIGHT/16 + 25; j <= HEIGHT; j += HEIGHT/16)
 			g.fillRect(25, j-1, WIDTH, 2);
 		g.setColor(Color.pink);
-		for (int k = 0; k < board.getBoard().length; k++) 
-			for (int l = 0; l < board.getBoard()[0].length; l++) 
+		for (int k = 0; k < board.getBoard().length; k++)
+			for (int l = 0; l < board.getBoard()[0].length; l++)
 				if (board.getBoard()[k][l] == 1)
 					g.fillRect((l+1)  * (HEIGHT/16) - 23, (k+1) * (WIDTH/10) - 23, 46, 46);
-		
-			
+
+
 		handler.render(g);
-		
+
 		g.dispose();
 		bs.show();
 	}
 
 	public static void main(String args[]) {
 		new MainGame();
-		
+
 	}
 }
-
