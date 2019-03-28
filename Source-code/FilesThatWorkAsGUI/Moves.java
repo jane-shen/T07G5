45e04586a365937a788729470104fd069f1c8f6b
davedino43 <43692615+davedino43@users.noreply.github.com>
2019-03-28 17:12:40 -0600

    Add files via upload

--- Source-code/FilesThatWorkAsGUI/Moves.java
+++ Source-code/FilesThatWorkAsGUI/Moves.java
@@ -1,20 +1,25 @@
-
 public class Moves {
 
 	public static void makeMove(MainGame game) {
 		switch (game.getBoard().getDirection()) {
+
+			//responsible for the block to move left
 		case "left":
 			if (!game.getBoard().leftCollision(game.getMainShape()))
 				game.getBoard().moveLeft(game.getMainShape());
 			game.getBoard().setDirection("");
 			game.getBoard().print2D();
 			break;
+
+			//responsible for the block to move right
 		case "right":
 			if (!game.getBoard().rightCollision(game.getMainShape()))
 				game.getBoard().moveRight(game.getMainShape());
 			game.getBoard().setDirection("");
 			game.getBoard().print2D();
 			break;
+
+			//responsible for the block to move down
 		case "down":
 			if (!game.getBoard().bottomCollision(game.getMainShape()))
 				game.getBoard().moveDown(game.getMainShape());
@@ -23,6 +28,8 @@ public class Moves {
 			game.getBoard().setDirection("");
 			game.getBoard().print2D();
 			break;
+
+			//responsible for the block to move all the way down until a it hits a block
 		case "space":
 			while (!game.getBoard().bottomCollision(game.getMainShape()))
 				game.getBoard().moveDown(game.getMainShape());
@@ -39,6 +46,8 @@ public class Moves {
 				MainGame.state = MainGame.GameState.ENDGAME;
 			game.getBoard().setDirection("");
 			break;
+
+
 		case "up":
 			if (!game.getBoard().bottomCollision(game.getMainShape()))
 				game.getBoard().rotateLeft(game.getMainShape(), game.getOrigShape());
@@ -47,6 +56,8 @@ public class Moves {
 			game.getBoard().setDirection("");
 			game.getBoard().print2D();
 			break;
+
+			
 		case "enter":
 			game.enterPressed();
 			game.getBoard().setDirection("");
@@ -69,5 +80,5 @@ public class Moves {
 				MainGame.state = MainGame.GameState.ENDGAME;
 		}
 	}
-	
+
 }
